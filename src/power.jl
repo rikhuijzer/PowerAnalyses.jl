f3d533e89f4dcbefe91824905f3fdc412b45c21d
rikhuijzer <rikhuijzer@pm.me>
2021-10-26 16:01:05 +0200

    Refactor

--- src/power.jl
+++ src/power.jl
@@ -1,42 +1,42 @@
 """
-    _power(d1::UnivariateDistribution, d2::UnivariateDistribution, alpha::Real, tail::Tail)
+    _power(d0::UnivariateDistribution, d1::UnivariateDistribution, alpha::Real, tail::Tail)
 
-Return `power` for which `quantile(d1, alpha) == quantile(d2, beta)` and `beta = 1 - power`.
+Return `power` for which `quantile(d0, alpha) == quantile(d1, beta)` and `beta = 1 - power`.
 """
-function _power(d1::UnivariateDistribution, d2::UnivariateDistribution, alpha::Real, tail::Tail)
+function _power(d0::UnivariateDistribution, d1::UnivariateDistribution, alpha::Real, tail::Tail)
     right_tail = tail == one_tail ? alpha : alpha / 2
     except_right_tail = 1 - right_tail
-    critical_value = quantile(d1, except_right_tail)
-    beta = cdf(d2, critical_value)
+    critical_value = quantile(d0, except_right_tail)
+    beta = cdf(d1, critical_value)
     return 1 - beta
 end
 
 """
-    _alpha(d1::UnivariateDistribution, d2::UnivariateDistribution, power::Real, tail::Tail)
+    _alpha(d0::UnivariateDistribution, d1::UnivariateDistribution, power::Real, tail::Tail)
 
-Return `alpha` for which `quantile(d1, alpha) == quantile(d2, beta)` and `beta = 1 - power`.
+Return `alpha` for which `quantile(d0, alpha) == quantile(d1, beta)` and `beta = 1 - power`.
 """
-function _alpha(d1::UnivariateDistribution, d2::UnivariateDistribution, power::Real, tail::Tail)
+function _alpha(d0::UnivariateDistribution, d1::UnivariateDistribution, power::Real, tail::Tail)
     beta = 1 - power
-    critical_value = quantile(d2, beta)
+    critical_value = quantile(d1, beta)
     @show critical_value
-    except_right_tail = cdf(d1, critical_value)
+    except_right_tail = cdf(d0, critical_value)
     right_tail = 1 - except_right_tail
     return tail == one_tail ? right_tail : 2 * right_tail
 end
 
-_distribution_parameters(T::IndependentSamplesTTest; n) = n - 2 # n1 + n2 - 2
-_distribution_parameters(T::PointBiserialTTest; n) = n - 2
-_distribution_parameters(T::TTest; n) = n - 1
-_distribution_parameters(T::ConstantVarianceChisqTest; n) = n - 1
-_distribution_parameters(T::ChisqTest; n) = T.df
-# _distribution_parameters(T::ANOVATest; n) = (T.n_groups - 1, (n - 1) * T.n_groups)
+_distribution_parameters(T::IndependentSamplesTTest, n) = n - 2 # n1 + n2 - 2
+_distribution_parameters(T::PointBiserialTTest, n) = n - 2
+_distribution_parameters(T::TTest, n) = n - 1
+_distribution_parameters(T::ConstantVarianceChisqTest, n) = n - 1
+_distribution_parameters(T::ChisqTest, n) = T.df
+# _distribution_parameters(T::ANOVATest, n) = (T.n_groups - 1, (n - 1) * T.n_groups)
 
-_noncentrality_parameter(T::IndependentSamplesTTest; es, n) = sqrt(n / 2) * es
-_noncentrality_parameter(T::PointBiserialTTest; es, n) = sqrt(es^2 / (1 - es^2)) * sqrt(n)
-_noncentrality_parameter(T::TTest; es, n) = sqrt(n) * es
-_noncentrality_parameter(T::ChisqTest; es, n) = n * es^2
-# _noncentrality_parameter(T::ANOVATest; es, n) = n * es^2 * T.n_groups
+_noncentrality_parameter(T::IndependentSamplesTTest, es, n) = sqrt(n / 2) * es
+_noncentrality_parameter(T::PointBiserialTTest, es, n) = sqrt(es^2 / (1 - es^2)) * sqrt(n)
+_noncentrality_parameter(T::TTest, es, n) = sqrt(n) * es
+_noncentrality_parameter(T::ChisqTest, es, n) = n * es^2
+# _noncentrality_parameter(T::ANOVATest, es, n) = n * es^2 * T.n_groups
 
 _distribution(T::TTest) = NoncentralT
 _distribution(T::ChisqTest) = NoncentralChisq
@@ -45,20 +45,34 @@ _distribution(T::FTest) = NoncentralF
 _tail(T::TTest) = T.tail
 _tail(T::StatisticalTest) = one_tail
 
-function _distributions(T::ConstantVarianceChisqTest; v, es, n)
-    scale = es
+"""
+    _null_distribution(T::StatisticalTest, v)
+
+Return the distribution for the null hypothesis of test `T` with distribution parameters `v`.
+"""
+function _null_distribution(T::StatisticalTest, v)
+    D = _distribution(T)
+    d0 = D(v..., 0)
+    return d0
+end
+
+"""
+    _alternative_distribution(T::StatisticalTest, v; es, n)
+
+Return the distribution for the null hypothesis of test `T` with distribution parameters `v`.
+"""
+function _alternative_distribution(T::StatisticalTest, v, es, n)
+    D = _distribution(T)
+    λ = _noncentrality_parameter(T, es, n)
+    d1 = D(v..., λ)
+    return d1
+end
+
+function _alternative_distribution(T::ConstantVarianceChisqTest, v, es, n)
     D = _distribution(T)
     d1 = D(v..., 0)
     d2 = LocationScale(v, es, d1)
-    return d1, d2
-end
-
-function _distributions(T::StatisticalTest; v, es, n)
-    D = _distribution(T)
-    d1 = D(v..., 0)
-    λ = _noncentrality_parameter(T; es, n)
-    d2 = D(v..., λ)
-    return d1, d2
+    return d2
 end
 
 """
@@ -66,9 +80,10 @@ end
 
 """
 function get_power(T::StatisticalTest; es::Real, alpha::Real, n)
-    v = _distribution_parameters(T; n)
-    d1, d2 = _distributions(T; v, es, n)
-    return _power(d1, d2, alpha, _tail(T))
+    v = _distribution_parameters(T, n)
+    d0 = _null_distribution(T, v)
+    d1 = _alternative_distribution(T, v, es, n)
+    return _power(d0, d1, alpha, _tail(T))
 end
 
 """
@@ -76,9 +91,10 @@ end
 
 """
 function get_alpha(T::StatisticalTest; es::Real, power::Real, n)
-    v = _distribution_parameters(T; n)
-    d1, d2 = _distributions(T; v, es, n)
-    return _alpha(d1, d2, power, _tail(T))
+    v = _distribution_parameters(T, n)
+    d0 = _null_distribution(T, v)
+    d1 = _alternative_distribution(T, v, es, n)
+    return _alpha(d0, d1, power, _tail(T))
 end
 
 """
